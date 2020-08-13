1ed8c6363f12870d06e42f335026a49609e9d10d
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-13 20:05:34 +0900

    2-11-2

--- 2/2-11/2-11-1.rb
+++ 2/2-11/2-11-1.rb
@@ -1,2 +1,39 @@
 def greeting(country)
 	if country =='japan'
+		'こんにちは'
+	else
+		'hello'
+	end
+end
+
+greeting
+
+greeting('us')
+
+greeting('us','japan')
+
+def greeting(country = 'japan')
+	if country == 'japan'
+		'こんにちは'
+	else
+		'hello'
+	end
+end
+
+greeting
+greeting('us')
+
+def default_args(a,b,c=0,d=0)
+	"a=#{a},b=#{b},c=#{c},d=#{d}"
+end
+default_args(1,2)
+default_args(1,2,3)
+default_args(1,2,3,4)
+
+def foo(time=Time.now,message =bar)
+	puts "time: #{time},message: #{message}"
+end
+def bar
+	'BAR'
+end
+foo
