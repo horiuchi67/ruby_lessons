5c63283f422515a30be30e1e377e38f4c3173667
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-15 23:16:37 +0900

    4-2-2

---
+++ 4/4-2-2.rb
@@ -0,0 +1,20 @@
+a, b = 1,2
+a
+b
+a, b = [1, 2]
+a
+b
+c, d =[10]
+c
+d
+e, f = [100,200,300]
+e
+f
+
+14.divmod(3)
+
+quo_rem = 14.divmod(3)
+"商=#{quo_rem[0]}, 余り=#{quo_rem[1]}"
+
+quotient, remainder = 14.divmod(3)
+"商= #{quotient}, 余り=#{remainder}"
