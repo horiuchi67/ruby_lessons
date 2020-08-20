7607fd316c774e57f3353a83590af6434b94f948
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-20 23:35:19 +0900

    4-7-8

---
+++ 4/4-7-6.rb
@@ -0,0 +1,12 @@
+a = []
+a.push(1)
+
+a = []
+b = [2,3]
+a.push(1)
+a.push(b)
+
+a = []
+b = [2,3]
+a.push(1)
+a.push(*b)
---
+++ 4/4-7-7.rb
@@ -0,0 +1,5 @@
+def greeting(*names)
+	"#{names.join('と')、こんにちは"
+end
+greeting('田中さん')
+greeting('田中さん','鈴木さん')
