3ac2fb751a6bde3bb8bd70583b013b4bf398791d
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-23 23:29:24 +0900

    4-9-1

--- 4/4-8-7.rb
+++ 4/4-8-7.rb
@@ -1,6 +1,6 @@
 a = [1,2,3]
 a.delete(100)
-a.delete(100) do 
+a.delete(100) do
 	'NG'
 end
 a.delete 100 do
---
+++ 4/4-9-1.rb
@@ -0,0 +1,8 @@
+sum = 0
+
+5.times { |n| sum += n}
+sum
+
+sum = 0
+5.times {sum += 1}
+sum
