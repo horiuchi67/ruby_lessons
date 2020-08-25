e9963553b7cad97dc42c0c1008b017da512c9675
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-25 23:31:13 +0900

    4-9-6

---
+++ 4/4-9-6.rb
@@ -0,0 +1,20 @@
+while true
+
+end
+
+loop do
+
+end
+
+numbers = [1,2,3,4,5]
+loop do
+	n = numbers.sample
+	puts n
+	break if n == 5
+end
+
+while true
+	n = numbers.sample
+	puts n
+	break if n == 5
+end
