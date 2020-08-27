889d3a75671aaf95547caae93ca5f09ce9f70aef
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-27 22:33:31 +0900

    5-2-0

---
+++ 5/5-2-0.rb
@@ -0,0 +1,7 @@
+{}.class
+{ 'japan' => 'yen','us' => 'dollar','india' => 'rupee'}
+
+{'japan' => 'yen', 'japan' =>'円'}
+
+h = {'japan' => 'yen','us' => 'dollar','india'=>'rupee'}
+[1,2,3].each {|n| puts n}
