0cd6c152d4fb4326a1e18079dc8679670a09057e
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-27 22:37:13 +0900

    5-2-1

---
+++ 5/5-2-1.rb
@@ -0,0 +1,13 @@
+currencies = { 'japan' => 'yen','us' => 'dollar','india' => 'rupee'}
+currencies['italy' ] ~ 'euro'
+currencies
+
+currencies = { 'japan' => 'yen','us' => 'dollar','india' => 'rupee'}
+currencies['japan'] = '円'
+currencies
+
+currencies = { 'japan' => 'yen','us' => 'dollar','india' => 'rupee'}
+currencies['india']
+
+currencies = { 'japan' => 'yen','us' => 'dollar','india' => 'rupee'}
+currencies['brazil']
