ac596e996b8b267a3bd09df5ad5ad8d74d25ebae
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-29 23:14:25 +0900

    5-4-1

---
+++ 5/5-4-1.rb
@@ -0,0 +1,11 @@
+currencies = {:japan => 'yen', :us => 'dollar', :india => 'rupee'}
+currencies[:us]
+
+currencies[:italy] = 'euro'
+
+currencies = {japan: 'yen', us: 'dollar', india: 'rupee'}
+currencies[:us]
+
+{japan: :yen, us: :dollar, india: :rupee}
+
+{ :japan => :yen, :us => :dollar, :india => :rupee}
