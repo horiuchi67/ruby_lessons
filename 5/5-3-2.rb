51d77f41a345ddf659d590da26c0e9fea17d3f76
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-29 23:10:09 +0900

    5-3-2

---
+++ 5/5-3-2.rb
@@ -0,0 +1,8 @@
+currencies = {'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
+currencies['japan']
+
+currencies = {:japan => 'yen', :us => 'dollar', :india => 'rupee'}
+currencies[:japan]
+
+'apple'.methods
+:apple.methods
