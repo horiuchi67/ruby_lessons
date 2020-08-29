672265dbdeddc2ef4e14107c4c404892e1c77625
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-29 23:05:17 +0900

    5-3-1

---
+++ 5/5-3-1.rb
@@ -0,0 +1,21 @@
+:apple.class
+'apple'.class
+
+'apple' == 'apple'
+:apple == :apple
+
+:apple.object_id
+:apple.object_id
+:apple.object_id
+
+'apple'.object_id
+'apple'.object_id
+'apple'.object_id
+
+string = 'apple'
+
+string.upcase!
+string
+
+symbol = :apple
+symbol.upcase!
