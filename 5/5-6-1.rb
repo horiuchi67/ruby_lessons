b0e31ec13d7e228762d3970eeb9e5c26e5e56b90
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-31 22:04:14 +0900

    5-6-1

---
+++ 5/5-6-1.rb
@@ -0,0 +1,9 @@
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee'}
+currencies.keys
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee'}
+currencies.values
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee'}
+currencies.has_key?(:japan)
+currencies.has_key?(:italy)
