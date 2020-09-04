338d75e8748cf4df87b89a5f416a28b1875d6f82
horiuchi67 <1234mhoriuchi@gmail.com>
2020-09-04 10:40:15 +0900

    5-7-1

---
+++ 5/5-7-1.rb
@@ -0,0 +1,23 @@
+:apple
+:Apple
+:ruby_is_fun
+:okay?
+:welcome!
+:_secret
+:$dollar
+:@at_mark
+
+:12345
+:ruby-is-fun
+:ruby is fun
+:()
+
+:'12345'
+:'ruby-is-fun'
+:'ruby is fun'
+:'(fun)'
+
+name = 'Alice'
+:"#{name.upcase}"
+
+hash = {'abc': 123}
