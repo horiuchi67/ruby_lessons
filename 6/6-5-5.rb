016e2e57ed50f3b28c6d40699b75b4faef18f7a4
horiuchi67 <1234mhoriuchi@gmail.com>
2020-09-10 08:53:03 +0900

    6-5-5

---
+++ 6/6-5-5.rb
@@ -0,0 +1,14 @@
+text = '私の誕生日は1977年7月17日です。'
+
+text =- /(\d+)年(\d+)月(\d+)日/
+
+Regexp.last_match
+
+Regexp.last_match(0)
+
+Regexp.last_match(1)
+Regexp.last_match(2)
+Regexp.last_match(3)
+
+Regexp.last_match(-1)
+
