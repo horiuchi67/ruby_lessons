44eebc31c82a7c98033e0af82d0860b1651f62fe
horiuchi67 <1234mhoriuchi@gmail.com>
2020-09-12 21:05:25 +0900

    7-2-1-5

--- 7/7-2-1.rb
+++ 7/7-2-1.rb
@@ -41,3 +41,11 @@ end
 users = []
 users << User.new('Alice', 'Ruby', 20)
 users << User.new('Bob', 'Python', 30)
+
+def full_name(user)
+	"#{user.first_name} #{user.last_name}"
+end
+
+users.each do |user|
+	puts "氏名: #{full_name(user)、年齢: #{user.age}"
+end
