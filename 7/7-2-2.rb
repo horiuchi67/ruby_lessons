506e08c695d8af4f3327af4ba4b5c4ead9226a90
horiuchi67 <1234mhoriuchi@gmail.com>
2020-09-14 23:34:48 +0900

    7-2-2-3

--- 7/7-2-2.rb
+++ 7/7-2-2.rb
@@ -7,3 +7,14 @@ bob.full_name
 
 user = User.new('Alice', 'Ruby', 20)
 user.first_name
+
+user = User.new('Alice', 'Ruby', 20)
+user.first_name
+
+class User
+	attr_accessor :first_name
+end
+user = User.new('Alice', 'Ruby', 20)
+user.first_name
+user.first_name = 'ありす'
+user.first_name
